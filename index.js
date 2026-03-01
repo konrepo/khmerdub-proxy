1b20eba7a84ce8abee5c3754c2676eb8ea184d72
konrepo <37514522+konrepo@users.noreply.github.com>
2026-03-01 17:16:40 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "khmerdub-proxy",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.13.5",
+    "express": "^4.18.2"
+  }
+}
